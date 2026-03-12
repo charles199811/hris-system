495d6fb88e883ae450ef7e97ac1a34366f7f2232
Charles Rodrigo <charlesrodrio@gmail.com>
2026-03-12 16:31:41 +0000

    update UI v3

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -16,10 +16,18 @@ const AdminUserUpdatePage = async (props: {
 
   if (!user) notFound();
 
+  const formUser = {
+    id: user.id,
+    name: user.name ?? "",
+    email: user.email,
+    role: user.role,
+    country: user.country ?? undefined,
+  };
+
   return (
     <div className="space-y-8 max-w-lg mx-auto">
       <h1 className="h2-bold">Update User</h1>
-      <UpdateUserForm user={user} />
+      <UpdateUserForm user={formUser} />
     </div>
   );
 };
